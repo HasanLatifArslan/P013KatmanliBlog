5a101e6c7932b60dca08274fbb364916069db8a9
HasanLatifArslan <126615039+HasanLatifArslan@users.noreply.github.com>
2023-05-05 18:39:59 +0300

    Add project files.

---
+++ P013KatmanliBlog.Core/Entities/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P013KatmanliBlog.Core.Entities
+{
+	public class Post : IEntity
+	{
+        public int Id { get; set; }
+    }
+}
